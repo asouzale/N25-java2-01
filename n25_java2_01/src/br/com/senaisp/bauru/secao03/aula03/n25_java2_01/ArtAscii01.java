fa73bdbc4a1d8caded6cbd6058bd1102d4c32475
Leticia <leeh.souza16@gmail.com>
2025-07-30 21:50:19 -0300

    Aula 02

---
+++ n25_java2_01/src/br/com/senaisp/bauru/secao03/aula03/n25_java2_01/ArtAscii01.java
@@ -0,0 +1,17 @@
+package n25_java2_01;
+
+public class ArtAscii01 {
+
+	public static void main(String[] args) {
+		System.out.println("  ████  ");
+        System.out.println(" █    █ ");
+        System.out.println("█ █  █ █");
+        System.out.println("█      █");
+        System.out.println("█ █  █ █");
+        System.out.println("█  ██  █");
+        System.out.println(" █    █ ");
+        System.out.println("  ████  ");
+	}
+	
+
+}
